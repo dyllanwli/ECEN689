39509d70b6428a1cd2302d40daf2cf8970aaa922
dyllanwli <messsking@gmail.com>
2021-04-28 16:37:02 -0500

    add paraiRL

---
+++ ParlaiRL/agents.py
@@ -0,0 +1,332 @@
+
+import torch
+
+from parlai.core.agents import (
+    _load_opt_file, 
+    get_agent_module, 
+    add_task_flags_to_agent_opt,
+    get_task_module)
+
+from parlai.core.torch_agent import AttrDict
+
+from parlai.core.torch_generator_agent import (
+    TorchGeneratorModel,
+    TorchGeneratorAgent)
+
+from parlai.core.utils import padded_tensor
+from parlai.core.agents import _load_opt_file
+
+from torch.autograd import Variable, backward
+from torch.distributions import Categorical
+from torch.nn import Module
+
+
+from os.path import isfile
+
+
+class Output(AttrDict):
+
+    def __init__(self, text=None, text_candidates=None, 
+                 text_vec=None, **kwargs):
+        """"""
+        super().__init__(
+            text=text, 
+            text_candidates=text_candidates, 
+            text_vec=text_vec,
+            **kwargs)
+
+
+def decode_probabilistic(model, encoder_states, batch_size, 
+                         max_len, num_det_steps):
+    """
+    Probabilistic search.
+    Arguments:
+        encoder_states: Output of the encoder model.
+        batch_size: Batch size. Because encoder_states is 
+            model-specific, it cannot infer this automatically.
+        max_len: Maximum decoding length.
+        num_det_steps: Number of deterministic iterations.
+    """
+    step_output = model._starts(batch_size)
+    incr_state = None
+    logits = []
+    for step in range(max_len):
+        scores, incr_state = model.decoder(
+            step_output, encoder_states, incr_state)
+        scores = scores[:, -1:, :]
+        scores = model.output(scores)
+
+        if step < num_det_steps:
+            _, preds = scores.max(dim=-1)
+        else:
+            distribution = Categorical(scores)
+            preds = distribution.sample()
+        
+        logits.append(scores)
+        step_output = torch.cat([step_output, preds], dim=1)  # pylint: disable=no-member
+
+        # check if everyone has generated an end token
+        all_finished = (
+            ((step_output == model.END_IDX).sum(dim=1) > 0)
+                .sum()
+                .item()
+            ) == batch_size
+
+        if all_finished:
+            break
+
+    logits = torch.cat(logits, 1)  # pylint: disable=no-member
+
+    return logits, step_output
+
+
+def compute_det_steps(model, step, max_len=None):
+    """
+    Computes the number of deterministic steps for
+    curicullum learning.
+    
+    Arguments:
+        model: ``TorchGeneratorModel`` type model instance.
+        step: int, the number of performed steps.
+        max_len: maximum length of a sequence.    
+    """
+    # TODO implement valid det step calculation
+    default_num_dep_step = min(max_len // 5, 3) if max_len else 3
+
+    return model.opt.get('num_det_step', default_num_dep_step)
+
+
+def forward(self, *inputs, ys=None, cand_params=None, 
+            prev_enc=None, maxlen=None, bsz=None, 
+            step=None, use_probabilistic_decode=False):
+    """
+    Calculates the forward pass for the model. This function
+    is assigned to `TorchGeneratorModel` instances with descriptor 
+    protocol to replace the `decode_greedy` function with 
+    `decode_probabilistic`.
+    """
+    if ys is not None:
+        # TODO: get rid of longest_label
+        # keep track of longest label we've ever seen
+        # we'll never produce longer ones than that 
+        # during prediction
+        self.longest_label = max(self.longest_label, ys.size(1))
+
+    # use cached encoding if available
+    encoder_states = (
+        prev_enc if prev_enc is 
+        not None else self.encoder(*inputs)
+    )
+
+    if ys is not None:
+        # use teacher forcing
+        scores, preds = self.decode_forced(encoder_states, ys)
+    else:
+        if use_probabilistic_decode:
+            scores, preds = self.decode_probabilistic(
+                encoder_states,
+                bsz,
+                maxlen or self.longest_label,
+                compute_det_steps(self, step, maxlen))
+        else:
+            scores, preds = self.decode_greedy(
+                encoder_states,
+                bsz,
+                maxlen or self.longest_label)
+
+    return scores, preds, encoder_states
+
+
+def replace_forward_method(model):
+    """
+    Extends the ``torch.nn.Module`` type model instance
+    by adding/replacing bound methods.
+    """
+    assert model is not None, ('Model must be initialized by '
+                               '``build_model`` method.')
+
+    model.forward = forward.__get__(model)
+
+
+def get_agent_type(opt):
+    """
+    Returns the type of model agent, specified by --model and
+    --model_file.
+    """
+    model_file = opt['model_file']
+    optfile = model_file + '.opt'
+    if isfile(optfile):
+        new_opt = _load_opt_file(optfile)
+        if 'batchindex' in new_opt:
+            del new_opt['batchindex']
+        if opt.get('override'):
+            for k, v in opt['override'].items():
+                if str(v) != str(new_opt.get(k, None)):
+                    print(
+                        "[ warning: overriding opt['{}'] to {} ("
+                        "previously: {} )]".format(
+                            k, v, new_opt.get(k, None)))
+                new_opt[k] = v
+        for k, v in opt.items():
+            if k not in new_opt:
+                new_opt[k] = v
+        new_opt['model_file'] = model_file
+        if (new_opt.get('dict_file') and not 
+                isfile(new_opt['dict_file'])):
+            raise RuntimeError(
+                'WARNING: Dict file does not exist, check '
+                'to make sure it is correct: {}'.format(
+                    new_opt['dict_file']))
+        model_class = get_agent_module(new_opt['model'])
+
+        return model_class
+    else:
+        return None
+
+
+def create_agent(opt):
+    """
+    Creates a new class, that extends the provided 
+    subclass of ``TorchGeneratorAgent`` with reinforcement
+    learning functionality.
+    """
+    torch_generator_agent_subclass = get_agent_type(opt)
+    loaded_opt = _load_opt_file(opt['model_file'] + '.opt')
+    opt.update(loaded_opt)
+    assert torch_generator_agent_subclass is not None
+
+    class RLTorchGeneratorAgent(torch_generator_agent_subclass):
+
+        def __init__(self, *args, shared=None, **kwargs):
+            super(RLTorchGeneratorAgent, self).__init__(
+                *args, shared=shared, **kwargs)
+            self.log_probs = [] # TODO implement shared actions
+
+        @torch.no_grad()
+        def sample_step(self, batch):
+            """
+            Sample a single batch of examples which will be used as 
+            labels for the train step.
+            """
+            self.model.eval()
+
+            _, preds, _ = self.model(
+                *self._model_input(batch), ys=batch.label_vec, 
+                use_probabilistic_decode=True)
+
+            self.add_labels(batch, preds)
+
+            return batch
+
+        def add_labels(self, batch, label_vecs):
+            """"""
+            labels = [self._v2t(l) for l in label_vecs]
+            ys, y_lens = padded_tensor(
+                label_vecs, self.NULL_IDX, self.use_cuda)
+                
+            batch.labels=labels
+            batch.label_vec=ys
+            batch.label_lengths=y_lens
+
+        def compute_log_prob(self, batch, track_metrics=True):
+            """"""
+            if batch.label_vec is None:
+                raise ValueError('Cannot compute loss without a label.')
+
+            model_output = self.model(
+                *self._model_input(batch), ys=batch.label_vec)
+            scores, preds, *_ = model_output
+            score_view = scores.view(-1, scores.size(-1))
+            log_prob = self.criterion(score_view, batch.label_vec.view(-1))
+
+            notnull = batch.label_vec.ne(self.NULL_IDX)
+            target_tokens = notnull.long().sum().item()
+            correct = ((batch.label_vec == preds) * notnull).sum().item()
+
+            if track_metrics:
+                self.metrics['correct_tokens'] += correct
+                self.metrics['nll_loss'] += log_prob.item()
+                self.metrics['num_tokens'] += target_tokens
+
+            log_prob /= target_tokens  # average loss per token
+
+            return log_prob
+
+        def train_step(self, batch):
+            """
+            Train on a single batch of examples.
+            """
+            try:
+                batch = self.sample_step(batch)
+                log_prob = self.compute_log_prob(batch)
+                self.metrics['loss'] += log_prob.item()
+                self.log_probs.append(log_prob)
+
+            except RuntimeError as e:
+                if 'out of memory' in str(e):
+                    print('| WARNING: ran out of memory, skipping batch. '
+                        'if this happens frequently, decrease batchsize or '
+                        'truncate the inputs to the model.')
+                    self.metrics['total_skipped_batches'] += 1
+                    # gradients are synced on backward, 
+                    # now this model is going to be
+                    # out of sync! catch up with the other workers
+                    self._init_cuda_buffer(8, 8, True)
+                else:
+                    raise e
+            
+            return Output(
+                text=batch.labels,
+                text_vec=batch.label_vec,
+                text_candidates=None)
+
+        def observe(self, observation):
+            """
+            Calculates the reward and copies the model if provided
+            additionally to the ``super().observe()``.
+            """
+            if observation.get('reward') is not None:
+                reward = observation['reward']
+                for log_prob in self.log_probs:
+                    loss = - log_prob * reward
+                    self.backward(loss)
+                
+                self.log_probs = []
+
+                for parameter in self.model.parameters():  # pylint: disable=access-member-before-definition
+                    if parameter.grad is not None:
+                        parameter.grad.data.clamp_(min=-5, max=5)
+                
+            if observation.get('model') is not None:
+                # Deepcopied and frozen clone of the model
+                self.model = observation['model']
+
+            return super(RLTorchGeneratorAgent, self).observe(
+                observation)
+
+        def match_batch(self, batch_reply, valid_inds, output=None):
+            """"""
+            batch_reply = super(RLTorchGeneratorAgent, self).match_batch(
+                batch_reply, valid_inds, output)
+
+            if getattr(output, 'text_vec', None) is not None:
+                for idx, val_idx in enumerate(valid_inds):
+                    batch_reply[val_idx]['text_vec'] = output.text_vec[idx]
+            
+            return batch_reply
+            
+        def build_model(self, *args, **kwargs):
+            super(RLTorchGeneratorAgent, self).build_model(
+                *args, **kwargs)
+            replace_forward_method(self.model)
+
+    return RLTorchGeneratorAgent(opt)
+
+
+def freeze_agent(agent):
+    """
+    Sets the gradient claculations off for a model.
+    """
+    for parameter in agent.model.parameters():
+        parameter.requires_grad = False
+    agent.model.eval()
---
+++ ParlaiRL/worlds.py
@@ -0,0 +1,153 @@
+
+import torch
+
+from torch.autograd import backward
+
+from parlai.core.worlds import MultiAgentDialogWorld
+from collections import namedtuple
+from itertools import chain
+
+
+Action = namedtuple('Action', ['id', 'action', 'responses'])
+"""
+A node in the action-response tree.
+:param id: str, the id of the actor.
+:param action: dict, containing the agent actions.
+:param responses: dict, containing the agent's response for the action.
+"""
+
+ACTIVE, STATIC = 'active', 'static'
+
+
+DULL_RESPONSES = [
+    {'labels': "i do n ' t know ."},
+    {'labels': 'why ?'},
+    {'labels': 'i am sorry .'},
+    {'labels': 'i see .'}
+]
+
+
+def calculate_dull_log_prob(resp_batch, agent):
+    """"""
+    model = agent.model
+    model.eval()
+    print(resp_batch)
+    batch = agent.batchify([
+        {**resp_batch, 
+        'labels': dull_response['labels'],
+        'labels_vec': agent._vectorize_text(dull_response['labels'])} for
+        dull_response in DULL_RESPONSES
+    ])
+    out = model(batch.text_vec, ys=batch.label_vec)
+    # generated response
+
+    notnull = batch.label_vec.ne(agent.NULL_IDX)
+    target_tokens = notnull.long().sum().item()
+
+    scores = out[0]
+    score_view = scores.view(-1, scores.size(-1))
+    loss = agent.criterion(score_view, batch.label_vec.view(-1))
+    loss = loss.sum().data.cpu().numpy() / target_tokens / len(DULL_RESPONSES)
+
+    return loss
+
+
+class RLDialogWorld(MultiAgentDialogWorld):
+
+    def __init__(self, opt, active_agent, static_agent, 
+                 teacher, shared=None):
+        """"""
+        self.id = 'RLDialogWorld'
+        self.episode_batch = None
+        self.active_agent = active_agent
+        self.static_agent = static_agent
+        agents = teacher + [static_agent, active_agent]
+
+        super(RLDialogWorld, self).__init__(opt, agents, shared)
+
+    def parley(self):
+        """"""
+        # Initial sentence from the dataset
+        initial_action = self.agents[0].act()
+
+        self.active_agent.zero_grad()
+        actions = self.rollout(initial_action)
+
+        with torch.no_grad():
+            reward = self.calculate_reward(actions, 1)
+
+        self.active_agent.observe({'reward': reward})
+        self.active_agent.update_params()
+
+    def iterate_reponses(self, action):
+        """"""
+        for response in action.responses:
+            yield {
+                'text': action.action['text'], 
+                'text_vec': action.action['text_vec'].cpu(), 
+                'labels': response.action['text'],
+                'labels_vec': response.action.get('text_vec', 
+                    self.static_agent._vectorize_text(
+                        response.action['text']).cpu())
+            }
+
+    def calculate_reward(self, action, weight):
+        """"""
+        reward = 0
+
+        if len(action.responses) == 0:
+            return 0
+
+        if action.id == ACTIVE:
+            for resp in self.iterate_reponses(action):
+                batch = self.active_agent.batchify([resp])
+
+                log_prob = self.static_agent.compute_log_prob(batch, False)
+
+                reward += log_prob - calculate_dull_log_prob(
+                    resp, self.static_agent)
+
+        # Reduce reward significance for next dialogue turns
+        weight = weight * self.opt['reward_decay']
+        for response in action.responses:
+            reward += self.calculate_reward(response, weight)
+           
+        return reward
+    
+    def rollout(self, initial_action):
+        """"""
+        def roll(action, num_rollouts):
+            """"""
+            if action.id == ACTIVE:
+                self.static_agent.observe(action.action)
+                act = self.static_agent.act()
+                act['text_vec'] = act['text_vec'].cpu()
+                static_action = Action(
+                    id=STATIC, 
+                    action=act, 
+                    responses=[])
+
+                action.responses.append(
+                    roll(static_action, num_rollouts))
+
+            else:
+                if num_rollouts == 0:
+                    return action
+
+                self.active_agent.observe(action.action)
+                for _ in range(self.opt['dialog_branches']):
+                    act = self.active_agent.act()
+                    act['text_vec'] = act['text_vec'].cpu()
+                    active_action = Action(
+                        id=ACTIVE,
+                        action=act, 
+                        responses=[])
+
+                    action.responses.append(
+                        roll(active_action, num_rollouts - 1))
+
+            return action
+
+        initial = Action(STATIC, initial_action, [])
+
+        return roll(initial, self.opt['dialog_rounds'])
